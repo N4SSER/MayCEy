0ef663e9118dfefce4075faa821dcc7723a4672e
N4SSER <nasserbrown@estudiantec.cr>
2022-05-21 01:04:20 -0600

    se agregaron BNFs

--- db.pl
+++ db.pl
@@ -1,14 +1,49 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-permiso(S0,S):-query(S0,S1),accion(S1,S).
+permiso(a,S0,S):-query(S0,S1),accion(a,S1,S).
 
-query(S0,S):-solicitud(S0,S1),determinante(S1,S).
+permiso(b,S0,S):-query(S0,S1),accion(b,S1,S).
+
+permiso(id,S0,S):-permiso(b,S0,S1),identificacion(S1,S).
+
+query(S0,S):-solicitud(S0,S1),nombre(q,S1,S).
 
 query(S0,S):-solicitud(S0,S).
 
-accion(S0,S):-linker(S0,S1),(verbo(a,S1,S);verbo(b,S1,S)).
+accion(a,S0,S):-linker(S0,S1),verbo(a,S1,S).
 
-accion(S0,S):-(verbo(a,S0,S);verbo(b,S0,S)).
+accion(a,S0,S):-verbo(a,S0,S).
+
+accion(b,S0,S):-linker(S0,S1),verbo(b,S1,S).
+
+accion(b,S0,S):-verbo(b,S0,S).
+
+identificacion(aeronave,S0,S):-sintagma_nominal(aeronave,S0,S1),sintagma_verbal(S1,S).
+
+sintagma_nominal(aeronave,S0,S):-determinante(S0,S1),nombre(aero,S1,S).
+
+sintagma_nominal(aeronave,S0,S):-nombre(aero,S0,S).
+
+sintagma_nominal(aeronave,id,S0,S):-nombre(aeronaves,S0,S).
+
+sintagma_verbal(S0,S):-verbo(ser,S0,S).
+
+sintagma_verbal(S0,S):-verbo(ser,S0,S1),sintagma_nominal(aeronave,id,S1,S).
+
+identificacion(matricula,S0,S):-sintagma_nominal(matricula,S0,S1),sintagma_verbal(S1,S).
+
+sintagma_nominal(matricula,S0,S):-determinante(S0,S1),nombre(id,S1,S).
+
+sintagma_nominal(matricula,S0,S):-nombre(id,S0,S).
+
+sintagma_nominal(matricula,id,S0,S):-nombre(matricula,S0,S).
+
+sintagma_verbal(S0,S):-verbo(ser,S0,S1),sintagma_nominal(matricula,id,S1,S).
+
+identificacion(S0,S):-identificacion(aeronave,S0,S1),identificacion(matricula,S1,S).
+
+identificacion(S0,S):-identificacion(matricula,S0,S1),identificacion(aeronave,S1,S).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 saludo([hola|S],S).
 
@@ -16,27 +51,84 @@ saludo([saludos|S],S).
 
 solicitud([solicito|S],S).
 
-determinante([permiso|S],S).
+nombre(q,[permiso|S],S).
 
-determinante([apoyo|S],S).
+nombre(q,[apoyo|S],S).
 
 solicitud([necesito|S],S).
 
 solicitud([quiero|S],S).
 
+determinante([mi|S],S).
+
+determinante([el|S],S).
+
+determinante([la|S],S).
+
 verbo(a,[despegar|S],S).
 
 verbo(a,[volar|S],S).
+
+verbo(ser,[es|S],S).
 
 verbo(b,[aterrizar|S],S).
 
 linker([para|S],S).
 
+nombre(aero,['aeronave'|S],S).
+
+nombre(aero,['avion'|S],S).
+
+nombre(id,['matricula'|S],S).
+
+nombre(id,['placa'|S],S).
+
+nombre(id,['id'|S],S).
+
+nombre(aeronaves,['Cessna'|S],S).
+
+nombre(aeronaves,['Beechcraft'|S],S).
+
+nombre(aeronaves,['Embraer Phenom'|S],S).
+
+nombre(aeronaves,['Boing 717'|S],S).
+
+nombre(aeronaves,['Embraer 190'|S],S).
+
+nombre(aeronaves,['AirBus A0'|S],S).
+
+nombre(aeronaves,['Boing 747'|S],S).
+
+nombre(aeronaves,['AirBus A340'|S],S).
+
+nombre(aeronaves,['Airbus A380'|S],S).
+
+nombre(pistas,['P1'|S],S).
+
+nombre(pistas,['P1-(Este a Oeste)'|S],S).
+
+nombre(pistas,['P'|S],S).
+
+nombre(pistas,['P-(Oeste a Este)'|S],S).
+
+nombre(matricula,['BNF00'|S],S).
+
+nombre(matricula,['BNF01'|S],S).
+
+nombre(matricula,['BNF02'|S],S).
+
+nombre(matricula,['BNF03'|S],S).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 despegar(Y):-
-    permiso(Y,[]),verbo(a,[X|S],S),member(X,Y),write('Tienes permiso para despegar').
+    permiso(a,Y,[]),write('Tienes permiso para despegar').
 
 aterrizar(Y):-
-    permiso(Y,[]),verbo(b,[X|S],S),member(X,Y),write('Tienes permiso para aterrizar').
+    (permiso(id,Y,[]);permiso(b,Y,[])),write('Tienes permiso para aterrizar').
+
+id(Y):-
+    ((identificacion(Y,[]),write('Contnuar...'));
+    (identificacion(aeronave,Y,[]),write('Ingrese su matricula: '),read(X),tokenize_atom(X,S),nombre(matricula,S,[]));
+    (identificacion(matricula,Y,[]),write('Ingrese su aeronave:'),read(X),tokenize_atom(X,S),nombre(aeronaves,S,[]))).
 
 main():-
-    (write('Que desea hacer: '),read(X), tokenize_atom(X, Y)),(despegar(Y);aterrizar(Y)).
+    (write('Que desea hacer: '),read(X), tokenize_atom(X, Y)),(despegar(Y);aterrizar(Y);id(Y)).
