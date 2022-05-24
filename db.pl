59751f9d9ed12a0383f98436c518b3bf31098529
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2022-05-24 00:00:44 -0600

    Separated DB and BNF

--- db.pl
+++ db.pl
@@ -1,49 +1,3 @@
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-permiso(a,S0,S):-query(S0,S1),accion(a,S1,S).
-
-permiso(b,S0,S):-query(S0,S1),accion(b,S1,S).
-
-permiso(id,S0,S):-permiso(b,S0,S1),identificacion(S1,S).
-
-query(S0,S):-solicitud(S0,S1),nombre(q,S1,S).
-
-query(S0,S):-solicitud(S0,S).
-
-accion(a,S0,S):-linker(S0,S1),verbo(a,S1,S).
-
-accion(a,S0,S):-verbo(a,S0,S).
-
-accion(b,S0,S):-linker(S0,S1),verbo(b,S1,S).
-
-accion(b,S0,S):-verbo(b,S0,S).
-
-identificacion(aeronave,S0,S):-sintagma_nominal(aeronave,S0,S1),sintagma_verbal(S1,S).
-
-sintagma_nominal(aeronave,S0,S):-determinante(S0,S1),nombre(aero,S1,S).
-
-sintagma_nominal(aeronave,S0,S):-nombre(aero,S0,S).
-
-sintagma_nominal(aeronave,id,S0,S):-nombre(aeronaves,S0,S).
-
-sintagma_verbal(S0,S):-verbo(ser,S0,S).
-
-sintagma_verbal(S0,S):-verbo(ser,S0,S1),sintagma_nominal(aeronave,id,S1,S).
-
-identificacion(matricula,S0,S):-sintagma_nominal(matricula,S0,S1),sintagma_verbal(S1,S).
-
-sintagma_nominal(matricula,S0,S):-determinante(S0,S1),nombre(id,S1,S).
-
-sintagma_nominal(matricula,S0,S):-nombre(id,S0,S).
-
-sintagma_nominal(matricula,id,S0,S):-nombre(matricula,S0,S).
-
-sintagma_verbal(S0,S):-verbo(ser,S0,S1),sintagma_nominal(matricula,id,S1,S).
-
-identificacion(S0,S):-identificacion(aeronave,S0,S1),identificacion(matricula,S1,S).
-
-identificacion(S0,S):-identificacion(matricula,S0,S1),identificacion(aeronave,S1,S).
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 saludo([hola|S],S).
 
@@ -118,17 +72,23 @@ nombre(matricula,['BNF01'|S],S).
 nombre(matricula,['BNF02'|S],S).
 
 nombre(matricula,['BNF03'|S],S).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+exit_words(1,[exit|S],S).
+
+%%%%
+%REGLAS
+exitkeyword(Y):-
+    exit_words(1,Y,[]),write('Saliendo...'), halt(0).
+
 despegar(Y):-
-    permiso(a,Y,[]),write('Tienes permiso para despegar').
+    permiso(a,Y,[]),write('Tienes permiso para despegar \n').
 
 aterrizar(Y):-
-    (permiso(id,Y,[]);permiso(b,Y,[])),write('Tienes permiso para aterrizar').
+    (permiso(id,Y,[]);permiso(b,Y,[])),write('Tienes permiso para aterrizar \n').
 
 id(Y):-
-    ((identificacion(Y,[]),write('Contnuar...'));
+    ((identificacion(Y,[]),write('Continuar...'));
     (identificacion(aeronave,Y,[]),write('Ingrese su matricula: '),read(X),tokenize_atom(X,S),nombre(matricula,S,[]));
     (identificacion(matricula,Y,[]),write('Ingrese su aeronave:'),read(X),tokenize_atom(X,S),nombre(aeronaves,S,[]))).
-
-main():-
-    (write('Que desea hacer: '),read(X), tokenize_atom(X, Y)),(despegar(Y);aterrizar(Y);id(Y)).
+saludo(Y):-
+    sintagma_nominal(s,Y,[]), write('Hola! En que te puedo ayuda?').
