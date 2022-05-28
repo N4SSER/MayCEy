46de33ac0b4cd89127f3eac002fc1f08eaad8ad8
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2022-05-27 22:28:51 -0600

    Implemented desicions

--- decisiones.pl
+++ decisiones.pl
@@ -14,26 +14,23 @@ indicar_pista(Direccion,Aeronave):-
     %Nave pequena. P1 libre
     (pequenas(Aeronave),not(ocupada('P1')),write('Se te ha asignado la pista P1 por 5 minutos'));
 
-    %Nave pequena, direccion este a oeste, P1 ocupada y P2-1 libre
-    (pequenas(Aeronave),ocupada('P1'),not(ocupada('P2-1')),este(Direccion),write('La pista P1 se encuentra llena, se te ha asignado la pista P2-1, por 5 minutos, por tu direccion'));
+    %Nave pequena,P1 ocupada y P2-1 libre
+    (pequenas(Aeronave),ocupada('P1'),not(ocupada('P2-1')),write('La pista P1 se encuentra llena, se te ha asignado la pista P2-1, por 5 minutos, por tu direccion'));
 
     %Nave pequena, direccion oeste a este, P1 ocupada y P2-2 libre
-    (pequenas(Aeronave),ocupada('P1'),not(ocupada('P2-2')),oeste(Direccion),write('La pista P1 se encuentra llena, se te ha asignado la pista P2-2, por 5 minutos, por tu direccion'));
+    (pequenas(Aeronave),ocupada('P1'),not(ocupada('P2-2')),write('La pista P1 se encuentra llena, se te ha asignado la pista P2-2, por 5 minutos, por tu direccion'));
 
     %Nave pequena, direccion este a oeste. P1 y P2-1  ocupadas, P3 libre
-    (pequenas(Aeronave),este(Direccion),ocupada('P1'),ocupada('P2-1'),not(ocupada('P3')),write('La pista P1 y P2-1 se encuentran llenas, se te ha asignado la pista P3, por 5 minutos'));
+    (pequenas(Aeronave),este(Direccion),ocupada('P1'),ocupada('P2-1'),ocupada('P2-2'), not(ocupada('P3')),write('La pista P1 y P2-1 se encuentran llenas, se te ha asignado la pista P3, por 5 minutos'));
     
-    %Nave pequena, P1, P2-1 y P2-2 ocupada, P3 libre
-    (pequenas(Aeronave),oeste(Direccion),ocupada('P1'),ocupada('P2-2'),not(ocupada('P3')),write('La pista P1 y P2-2 se encuentran llenas, se te ha asignado la pista P3, por 5 minutos'));
+    %Nave mediana. direccion este a oeste, P2-1 libre
+    (medianas(Aeronave),not(ocupada('P2-1')), write('Se te ha asignado la pista P2-1 por 5 minutos'));
 
     %Nave mediana. direccion este a oeste, P2-1 libre
-    (medianas(Aeronave),not(ocupada('P2-1')),este(Direccion), write('Se te ha asignado la pista P2-1 por 5 minutos'));
-
-    %Nave mediana. direccion este a oeste, P2-1 libre
-    (medianas(Aeronave),not(ocupada('P2-2')),oeste(Direccion), write('Se te ha asignado la pista P2-2 por 5 minutos'));
+    (medianas(Aeronave),not(ocupada('P2-2')), write('Se te ha asignado la pista P2-2 por 5 minutos'));
     
     %Nave mediana. P2-1 llena, P3 libre
-    (medianas(Aeronave),ocupada('P2-1'), not(ocupada('P3')), write('Se te ha asignado la pista P2-1'));
+    (medianas(Aeronave),ocupada('P2-1'),ocupada('P2-2'), not(ocupada('P3')), write('Se te ha asignado la pista P3 por 5 minutos'));
 
     %Nave grande. P3 libre
     (grandes(Aeronave),not(ocupada('P3')),write('Se te ha asignado la pista P3 por 5 minutos'));
